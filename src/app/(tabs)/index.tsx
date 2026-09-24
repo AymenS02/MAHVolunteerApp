b7fd4b751662f6d46edc6b611afcd0683d7df716
Aymen Shoteri <95427313+AymenS02@users.noreply.github.com>
2026-09-24 15:22:37 -0400

    fixed temp and pg.json

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 items-center justify-center">
-        <Text className="text-4xl font-bold">MAH Volunteers</Text>
+        <Text className="text-4xl text-red-600 font-bold">MAH Volunteers</Text>
       </View>
     </SafeAreaView>
   );
